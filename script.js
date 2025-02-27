95595c4c213f6cc473d10242ab037100cf745ccb
gyullee96 <gyullee96@naver.com>
2025-02-27 22:03:43 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('button');
+
+
+button.addEventListener('click',) //버튼 누를 때 실행 될 이벤트
+
+// 랜덤하게 뽑아 낼 자료
+
+// 자료를 랜덤하게 뽑게 해주는 코드
+
+// 그 자료를 출력해주는 코드
+
+// (265 랜덤컬러러)
